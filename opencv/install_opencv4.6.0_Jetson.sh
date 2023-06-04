b32f07ad82c5f23b9fa2f1f2c1aa3d663142ce33
Hori3538 <yu.m.h.94612@gmail.com>
2023-06-05 01:05:26 +0900

    minor modification

--- opencv/install_opencv4.6.0_Jetson.sh
+++ opencv/install_opencv4.6.0_Jetson.sh
@@ -29,17 +29,18 @@ sudo  apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::
 
 
 echo "Download opencv "${version}""
+cd ~
 curl -L https://github.com/opencv/opencv/archive/${version}.zip -o opencv-${version}.zip
 curl -L https://github.com/opencv/opencv_contrib/archive/${version}.zip -o opencv_contrib-${version}.zip
 unzip opencv-${version}.zip
 unzip opencv_contrib-${version}.zip
-rm opencv-${version}.zip opencv_contrib-${version}.zip
+rm -f opencv-${version}.zip opencv_contrib-${version}.zip
 cd opencv-${version}/
 
 
 echo "Build opencv "${version}""
-mkdir release
-cd release/
+mkdir build
+cd build/
 cmake -D WITH_CUDA=ON -D WITH_CUDNN=ON -D CUDA_ARCH_BIN="7.2,8.7" -D CUDA_ARCH_PTX="" -D OPENCV_GENERATE_PKGCONFIG=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j$(nproc)
 

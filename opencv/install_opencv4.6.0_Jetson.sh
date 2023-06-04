d11cf3df8d238288e2e0bcb937c67ab27d8fb648
Hori3538 <yu.m.h.94612@gmail.com>
2023-06-05 00:51:07 +0900

    add opencv install script

---
+++ opencv/install_opencv4.6.0_Jetson.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+version="4.6.0"
+
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+set_password()
+{
+    if [ ! ${password} ]
+    then
+        printf "password: "
+        read -s password
+    fi
+}
+set_password
+
+echo "Install requirement"
+
+echo "$password" | sudo -S apt-get update
+sudo aptget -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+sudo  apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
+    build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev \
+    libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
+    python3.8-dev python-dev python-numpy python3-numpy \
+    libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev \
+    libv4l-dev v4l-utils qv4l2 v4l2ucp \
+    curl
+
+
+echo "Download opencv "${version}""
+curl -L https://github.com/opencv/opencv/archive/${version}.zip -o opencv-${version}.zip
+curl -L https://github.com/opencv/opencv_contrib/archive/${version}.zip -o opencv_contrib-${version}.zip
+unzip opencv-${version}.zip
+unzip opencv_contrib-${version}.zip
+rm opencv-${version}.zip opencv_contrib-${version}.zip
+cd opencv-${version}/
+
+
+echo "Build opencv "${version}""
+mkdir release
+cd release/
+cmake -D WITH_CUDA=ON -D WITH_CUDNN=ON -D CUDA_ARCH_BIN="7.2,8.7" -D CUDA_ARCH_PTX="" -D OPENCV_GENERATE_PKGCONFIG=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make -j$(nproc)
+
+
+echo "Install opencv "${version}""
+echo "$password" | sudo make install
+echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PYTHONPATH=/usr/local/lib/python3.8/site-packages/:$PYTHONPATH' >> ~/.bashrc
+source ~/.bashrc
+
+
+echo "** Install opencv "${version}" successfully"

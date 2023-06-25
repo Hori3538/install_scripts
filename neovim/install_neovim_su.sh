4b54e84f8fea58e68c880b959e5e0fbc8809674b
Hori3538 <yu.m.h.94612@gmail.com>
2023-06-26 02:59:01 +0900

    change install method apt to appimg

--- neovim/install_neovim_su.sh
+++ neovim/install_neovim_su.sh
@@ -6,7 +6,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-    neovim
+    nodejs
+
+cd ${SCRIPT_DIR}
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+ln -s ./nvim.appimage  /usr/bin/nvim
 
 cd ~/.config
 git clone https://github.com/Hori3538/nvim.git

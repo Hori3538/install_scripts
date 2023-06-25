9023b3cb47907f1ff576858d37ef999fc2120d6c
Hori3538 <yu.m.h.94612@gmail.com>
2023-06-26 04:14:26 +0900

    minor change

--- neovim/install_neovim_su.sh
+++ neovim/install_neovim_su.sh
@@ -6,13 +6,17 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-    nodejs wget
+    wget
 
 cd ${SCRIPT_DIR}
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
 mv nvim-linux64 /
 ln -sf /nvim-linux64/bin/nvim  /usr/bin/nvim
+
+# install nodejs
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
+apt-get install -y nodejs
 
 cd ~/.config
 git clone https://github.com/Hori3538/nvim.git

17176dd0048205e784c17f52e40da4daed3b11f2
Hori3538 <yu.m.h.94612@gmail.com>
2023-06-26 03:25:32 +0900

    minor update

--- neovim/install_neovim_su.sh
+++ neovim/install_neovim_su.sh
@@ -9,9 +9,9 @@ apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--fo
     nodejs
 
 cd ${SCRIPT_DIR}
-curl https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
-ln -s ./nvim.appimage  /usr/bin/nvim
+ln -sf ./nvim-linux64/bin/nvim  /usr/bin/nvim
 
 cd ~/.config
 git clone https://github.com/Hori3538/nvim.git

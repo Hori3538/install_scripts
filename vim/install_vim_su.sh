c4a6abab63390e599b7b2dcc547dab64df58aed5
Hori3538 <yu.m.h.94612@gmail.com>
2023-07-02 01:38:51 +0900

    add vim install scripts

---
+++ vim/install_vim_su.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+set_password()
+{
+    if [ ! ${password} ]
+    then
+        printf "password: "
+        read -s password
+    fi
+}
+set_password
+
+echo "$password" | sudo apt-get update
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
+    vim
+
+cd ~
+git clone https://github.com/Hori3538/vim_conf.git
+cd tmux_conf && ./reset_vim_conf.sh
+
+echo "alias v=vim" >> ~/.bashrc
+source ~/.bashrc
